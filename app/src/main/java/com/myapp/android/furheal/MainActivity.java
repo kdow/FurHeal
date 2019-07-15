0ef743628af648b760a7d047118372bfd8040357
kdow <kedownes@gmail.com>
2019-07-14 21:45:48 -0700

    Add main and weight activities

---
+++ app/src/main/java/com/myapp/android/furheal/MainActivity.java
@@ -0,0 +1,36 @@
+package com.myapp.android.furheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mWeightButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mWeightButton = (Button) findViewById(R.id.weight);
+
+        mWeightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToWeight();
+            }
+        });
+    }
+
+    private void goToWeight() {
+        Intent intent = new Intent(this, WeightActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/myapp/android/furheal/WeightActivity.java
@@ -0,0 +1,14 @@
+package com.myapp.android.furheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight);
+    }
+}
