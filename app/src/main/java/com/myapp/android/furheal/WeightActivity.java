ee0a185e68a1e499cbe3db69ff75e9934a74cca6
kdow <kedownes@gmail.com>
2019-07-16 21:38:26 -0700

    Add user input fields to add weight page

--- app/src/main/java/com/myapp/android/furheal/WeightActivity.java
+++ app/src/main/java/com/myapp/android/furheal/WeightActivity.java
@@ -2,13 +2,42 @@ package com.myapp.android.furheal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Date;
 
 public class WeightActivity extends AppCompatActivity {
 
+    private Date mDate;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight);
+
+        Button mAddWeightButton = (Button) findViewById(R.id.add_weight);
+
+        mAddWeightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAddWeight();
+            }
+        });
+    }
+
+    private void goToAddWeight() {
+        Intent intent = new Intent(this, AddWeightActivity.class);
+        startActivity(intent);
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
     }
 }
