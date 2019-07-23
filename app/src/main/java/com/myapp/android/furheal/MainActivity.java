3b3e8ae4a7f715030ad804cbaa1e9374283214d6
kdow <kedownes@gmail.com>
2019-07-23 10:13:28 -0700

    Revert "Add nav drawer"
    This reverts commit 4e7c04bceb1de1f9d9e9ce1f7ae6e0084ca63ec1.

--- app/src/main/java/com/myapp/android/furheal/MainActivity.java
+++ app/src/main/java/com/myapp/android/furheal/MainActivity.java
@@ -1,20 +1,13 @@
 package com.myapp.android.furheal;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
